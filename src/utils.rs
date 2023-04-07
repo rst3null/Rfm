5b6930ccd55bbfe65edac5b1bd686a4c6d503a05
rst3null <byte21c@gmail.com>
2023-04-07 09:15:46 +0900

    imprement multiply algorithm

--- src/utils.rs
+++ src/utils.rs
@@ -9,48 +9,47 @@ use std::cmp;
 /// なお、この関数がサポートする数は自然数と0のみ。
 ///この計算量はO(N)である。
 ///
-pub(crate) fn arbitrary_precision_add(lhs: &Vec<u64>, rhs: &Vec<u64>) -> Vec<u64> {
+pub(crate) fn arbitrary_precision_add(lhs: &[u64], rhs: &[u64]) -> Vec<u64> {
     let argsize = cmp::max(lhs.len(), rhs.len());
-    let mut array: Vec<u64> = Vec::with_capacity(argsize + 1); //桁上がりの範囲として+1の範囲を予約
-    let mut carry: u64 = 0; //桁上がり
+    let mut array: Vec<u64> = Vec::with_capacity(argsize + 1); 
+    let mut carry: u64 = 0; 
     for i_ in 0..argsize {
         let lhs_digit = *lhs.get(i_).unwrap_or(&0);
         let rhs_digit = *rhs.get(i_).unwrap_or(&0);
-        let result_add = lhs_digit.overflowing_add(rhs_digit); //加算処理
-        let result_number = result_add.0.overflowing_add(carry); //桁上がりを足して桁確定
+        let result_add = lhs_digit.overflowing_add(rhs_digit); 
+        let result_number = result_add.0.overflowing_add(carry); 
         carry = 0;
         if true == result_add.1 {
-            carry += 1; //加算による桁上がり
+            carry += 1; 
         }
         if true == result_number.1 {
-            carry += 1; //桁上がり処理による数字確定字の桁上がり
+            carry += 1; 
         }
         array.push(result_number.0);
     }
     if 0 < carry {
-        array.push(carry); //最後の桁上がりを処理する。
+        array.push(carry); 
     }
     return array;
 }
 
 
-/// 減算計算を行う
-/// Internal substitutor imprements.
-///
-/// # 引数 Arguments
-/// * 'lhs' - 左辺値
-/// * 'rhs' - 右辺値
-///
-/// # 戻り値 returns
-/// 戻り値は(Vec<u64>,bool)のタプルで返される
-/// 1. Vec<u64> 計算後の値
-/// 2. bool 符号(trueのとき負)
-///
-
-pub(crate) fn arbitrary_precision_sub(lhs: &Vec<u64>, rhs: &Vec<u64>) -> (Vec<u64>, bool) {
+/**  減算計算を行う
+ *  Internal substitutor imprements.
+ * 
+ *  # 引数 Arguments
+ *  * 'lhs' - 左辺値
+ *  * 'rhs' - 右辺値
+ * 
+ *  # 戻り値 returns
+ *  戻り値は(Vec<u64>,bool)のタプルで返される
+ *  1. Vec<u64> 計算後の値
+ *  2. bool 符号(trueのとき負)
+ */ 
+pub(crate) fn arbitrary_precision_sub(lhs: &[u64], rhs: &[u64]) -> (Vec<u64>, bool) {
     let array_len: usize = cmp::max(lhs.len(), rhs.len());
     let mut array_result: Vec<u64> = Vec::with_capacity(array_len);
-    let mut carry_down: u64 = 0; //桁下がり
+    let mut carry_down: u64 = 0; 
     for _i in 0..array_len {
         let rhs_digit = *rhs.get(_i).unwrap_or(&0);
         let lhs_digit = *lhs.get(_i).unwrap_or(&0);
@@ -67,18 +66,20 @@ pub(crate) fn arbitrary_precision_sub(lhs: &Vec<u64>, rhs: &Vec<u64>) -> (Vec<u6
     }
 
     return match carry_down {
-        0 => (cut_upper_zeros(array_result), false),
-        1 => {//キャリーが残ってしまった場合、補数表現になっているので元に戻す
-            //答えは負数()
-            return (cut_upper_zeros(to_min_complement(array_result)), true);
+        0 => (cut_upper_zeros(&array_result), false),
+        1 => {
+            return (cut_upper_zeros(&to_min_complement(&array_result)), true);
         }
         _ => panic!("Something wrong in substitution algorithm. Please contact to developer.\nERROR_TYPE:Multiple carry down occured."), //余分に桁が降りてきているので計算ミス
     };
 }
 
+
+
 ///最小の補数表現にする
 ///また、補数から絶対値に戻すこともできる。
-pub(crate) fn to_min_complement(mut array_result: Vec<u64>) -> Vec<u64> {
+pub(crate) fn to_min_complement(number: &[u64]) -> Vec<u64> {
+    let mut array_result = number.to_vec();
     let mut need_carry: bool = true;
     let mut first_zeros: bool = true;
     for i_ in 0..array_result.len() {
@@ -103,16 +104,20 @@ pub(crate) fn to_min_complement(mut array_result: Vec<u64>) -> Vec<u64> {
 ///上位桁の余った桁を除去する。
 ///cuting uppernumber zeros.
 ///
-pub(crate) fn cut_upper_zeros(mut number: Vec<u64>) -> Vec<u64> {
-    for _i in (1..number.len()).rev() {
-        if number[_i] == 0u64 {
-            number.remove(_i);
+pub(crate) fn cut_upper_zeros(number:&[u64]) -> Vec<u64> {
+    let mut num_mut = number.to_vec();
+    for _i in (1..num_mut.len()).rev() {
+        if num_mut[_i] == 0u64 {
+            num_mut.remove(_i);
         } else {
-            return number;
+            return num_mut;
         }
     }
-    return number;
+    return num_mut;
 }
+
+
+ 
 
 //指数法則と分割統治法から、32bitごとに分割して計算することで1桁の64進数同士の乗算を安全に行うことを証明することができる。
 //2^32 * 2^32 = 2^(32+32) = 2^64 
@@ -121,11 +126,128 @@ pub(crate) fn cut_upper_zeros(mut number: Vec<u64>) -> Vec<u64> {
 pub(crate) fn safe_multiply_digit_64bit(lhs:u64,rhs:u64) -> Vec<u64>{
     let lhs_sep:(u64,u64) = (lhs & 0xFFFFFFFFu64 , (lhs & 0xFFFFFFFF00000000u64)>>32 );
     let rhs_sep:(u64,u64) = (rhs & 0xFFFFFFFFu64 , (rhs & 0xFFFFFFFF00000000u64)>>32 );
-    //2の64乗の桁と、2の32乗の桁、2の0乗の計算が必要
     let res_2p64:u64 = lhs_sep.1 * rhs_sep.1;
     let res_2p32:u64 = lhs_sep.0 * rhs_sep.1 + lhs_sep.1 * rhs_sep.0;
     let res_2p0 :u64 = lhs_sep.0 * rhs_sep.0;
-    return vec![res_2p0+((res_2p32 & 0xFFFFFFFFu64)<<32),res_2p64 + ((res_2p32 & 0xFFFFFFFF00000000u64)>> 32)];
+    return cut_upper_zeros(&vec![res_2p0+((res_2p32 & 0xFFFFFFFFu64)<<32),res_2p64 + ((res_2p32 & 0xFFFFFFFF00000000u64)>> 32)]);
+}
+
+pub(crate) fn append_upper_zeros(number:&[u64],needed_size:usize) -> Vec<u64> {
+    let num_append_digits = needed_size-number.len();
+    return [number,&vec![0u64;num_append_digits]].concat();
+}
+
+
+///正の整数同士を乗算する関数
+///
+pub(crate) fn arbitrary_precision_mul(lhs:&[u64],rhs:&[u64]) -> Vec<u64>{
+    assert!(lhs.len()!=0 && rhs.len()!=0);
+
+    if &vec![0u64;lhs.len()] == lhs || &vec![0u64;rhs.len()] == rhs {
+        return vec![0u64];
+    }
+
+    if &vec![1u64] == &cut_upper_zeros(lhs) {
+        return rhs.to_vec();
+    }
+
+    if &vec![1u64] == &cut_upper_zeros(rhs) {
+        return lhs.to_vec();
+    }
+
+    if lhs.len() < rhs.len() {
+        return arbitrary_precision_mul(&append_upper_zeros(lhs,rhs.len()),rhs);
+    }
+    if lhs.len() > rhs.len() {
+        return arbitrary_precision_mul(lhs,&append_upper_zeros(rhs,lhs.len()));
+    }
+
+    let length = lhs.len();
+
+    match length {
+        1 => {
+            let result = safe_multiply_digit_64bit(lhs[0],rhs[0]);
+            return result;
+        },
+        _ => {
+            let split_point = length/2;
+            let lhs_split = (&lhs[0..split_point],&lhs[split_point..length]);
+            let rhs_split = (&rhs[0..split_point],&rhs[split_point..length]);
+
+            let mul_down = match lhs_split.0.len() {
+                0 =>panic!("Error in split logic. (generate zero sized array.)"),
+                1 =>safe_multiply_digit_64bit(lhs_split.0[0], rhs_split.0[0]),
+                _ =>arbitrary_precision_mul(&lhs_split.0, &rhs_split.0),
+            };
+            let mul_up = match lhs_split.1.len() {
+                0 =>panic!("Error in split logic. (generator zero sized array.)"),
+                1 =>safe_multiply_digit_64bit(lhs_split.1[0], rhs_split.1[0]),
+                _=>arbitrary_precision_mul(&lhs_split.1, &rhs_split.1),
+            };
+
+            let lhs_sub_result = arbitrary_precision_sub(lhs_split.1,lhs_split.0);
+            let rhs_sub_result = arbitrary_precision_sub(rhs_split.0,rhs_split.1);
+            dbg!(&rhs_sub_result);
+            let mul_karatsuba = arbitrary_precision_mul(
+                &lhs_sub_result.0,
+                &rhs_sub_result.0
+            );
+            let add_karatsuba = arbitrary_precision_add(&mul_down, &mul_up);
+            let karatsuba = match lhs_sub_result.1 ^ rhs_sub_result.1 {
+                true => arbitrary_precision_sub(&add_karatsuba, &mul_karatsuba),
+                false => (arbitrary_precision_add(&add_karatsuba, &mul_karatsuba),lhs_sub_result.1)
+            };
+            let lower = mul_down;
+            let middle = [vec![0u64;split_point],karatsuba.0].concat();
+            let upper = [vec![0u64;split_point*2],mul_up].concat();
+
+            let low_add_up = arbitrary_precision_add(&lower, &upper);
+            let result =  match karatsuba.1 {
+                false => cut_upper_zeros(&arbitrary_precision_add(&low_add_up, &middle)),
+                true => cut_upper_zeros(&arbitrary_precision_sub(&low_add_up, &middle).0) 
+            };
+            return result;
+        }
+    }
+    
+}
+
+#[cfg(test)]
+mod appdend_zeros_test{
+    use crate::utils::append_upper_zeros;
+
+    #[test]
+    fn test_append(){
+        assert_eq!(append_upper_zeros(&vec![1u64],3),vec![1u64,0u64,0u64]);
+    }
+}
+#[cfg(test)]
+mod mul_arbitrary_test{
+    use crate::utils::arbitrary_precision_mul;
+
+    #[test]
+    fn test_mul_by_zero(){
+        assert_eq!(arbitrary_precision_mul(&vec![1u64;3],&vec![0u64]),vec![0u64]);
+        assert_eq!(arbitrary_precision_mul(&vec![0u64],&vec![1u64;3]),vec![0u64]);
+    }
+
+    #[test]
+    fn test_mul_by_one(){
+        let lhs = vec![1u64,2u64,3u64];
+        assert_eq!(arbitrary_precision_mul(&lhs,&vec![1u64]),lhs);
+        assert_eq!(arbitrary_precision_mul(&vec![1u64],&lhs),lhs);
+    }
+    #[test]
+    fn test_bug_case(){
+        assert_eq!(arbitrary_precision_mul(&vec![1u64],&vec![1u64,3u64]),vec![1u64,3u64]);
+    }
+
+    #[test]
+    fn test_shift(){
+        assert_eq!(arbitrary_precision_mul(&vec![0u64,1u64], &vec![1234567u64]),vec![0u64,1234567u64]);
+        assert_eq!(arbitrary_precision_mul(&vec![1234567u64], &vec![0u64,1u64]),vec![0u64,1234567u64]);
+    }
+
 }
 
 #[cfg(test)]
@@ -136,6 +258,11 @@ mod mul_digit_test{
     #[test]
     fn test_multiply(){
         assert_eq!(safe_multiply_digit_64bit(u64::MAX,2u64),vec![u64::MAX-1u64,1]);
+    }
+
+    #[test]
+    fn mul_zero(){
+        assert_eq!(safe_multiply_digit_64bit(u64::MAX,0u64),vec![0u64]);
     }
 }
 
--- src/vul.rs
+++ src/vul.rs
@@ -1,15 +1,12 @@
+use crate::utils::*;
 ///Rust用の多倍長精度演算プロジェクトrfmです。
 ///
 /// 金融計算など、高精度な計算が必要な場面でpure rustで計算を実行します。
-
-use std::ops::{Add, Div, Mul, Neg, Sub};
-use crate::utils::*;
-
+use std::ops::*;
 
 ///rfmライブラリにおける整数型の表現です。
 ///Integer expression in rfm library.
 pub struct Integer {
-    
     ///整数の絶対値
     ///整数の絶対値を18446744073709551616進数で表記する配列
     ///この配列は絶対値を保持しており、補数表現をしてはならない。
@@ -20,54 +17,56 @@ pub struct Integer {
     sign: bool,
 }
 
+///rfmライブラリにおける有理数型の表現です。
+///有理数は2つの整数型を組み合わせた分数で表現されます。
+pub struct Rational {
+    positive: Integer,
+    inverse: Integer,
+}
+
 impl Add for Integer {
     type Output = Integer;
     fn add(self, rhs: Self) -> Self::Output {
-        match (self.sign, rhs.sign) {
-            //正の整数同士の加算
-            (false, false) => Integer {
-                sign: false,
-                abs_number: arbitrary_precision_add(&self.abs_number, &rhs.abs_number),
-            },
-            //負数同士の加算なので加算結果を負数にする。
-            (true, true) => Integer {
-                sign: true,
-                abs_number: arbitrary_precision_add(&self.abs_number, &rhs.abs_number),
-            },
-            //両辺に-1を掛けて減算し、最後に結果の符号を反転
-            (true, false) => {
-                let result_sub = arbitrary_precision_sub(&self.abs_number, &rhs.abs_number);
-                return Integer {
-                    sign: !result_sub.1,
-                    abs_number: result_sub.0,
-                };
-            }
-            //相手がマイナスなので引き算と等価
-            (false, true) => {
-                let result_sub = arbitrary_precision_sub(&self.abs_number, &rhs.abs_number);
-                return Integer {
-                    sign: result_sub.1,
-                    abs_number: result_sub.0,
-                };
-            }
-        }
+        add_router(&self,&rhs)
     }
 }
 
-impl Sub for Integer {
-    type Output = Integer;
-    fn sub(self, rhs: Self) -> Self::Output {
-        return self + -rhs;
+impl AddAssign for Integer {
+    fn add_assign(&mut self, other: Self) {
+        *self = add_router(self,&other);
     }
 }
-/* 
-impl Mul for Integer {
-    type Output = Integer;
-    fn mul(self, rhs: Self) -> Self::Output {
-        
+
+fn add_router(lhs: &Integer, rhs: &Integer) -> Integer {
+    match (lhs.sign, rhs.sign) {
+        //正の整数同士の加算
+        (false, false) => Integer {
+            sign: false,
+            abs_number: arbitrary_precision_add(&lhs.abs_number, &rhs.abs_number),
+        },
+        //負数同士の加算なので加算結果を負数にする。
+        (true, true) => Integer {
+            sign: true,
+            abs_number: arbitrary_precision_add(&lhs.abs_number, &rhs.abs_number),
+        },
+        //両辺に-1を掛けて減算し、最後に結果の符号を反転
+        (true, false) => {
+            let result_sub = arbitrary_precision_sub(&lhs.abs_number, &rhs.abs_number);
+            return Integer {
+                sign: !result_sub.1,
+                abs_number: result_sub.0,
+            };
+        }
+        //相手がマイナスなので引き算と等価
+        (false, true) => {
+            let result_sub = arbitrary_precision_sub(&lhs.abs_number, &rhs.abs_number);
+            return Integer {
+                sign: result_sub.1,
+                abs_number: result_sub.0,
+            };
+        }
     }
 }
-*/
 
 impl Neg for Integer {
     type Output = Integer;
@@ -77,6 +76,72 @@ impl Neg for Integer {
             sign: !self.sign,
             abs_number: self.abs_number,
         };
+    }
+}
+
+impl Sub for Integer {
+    type Output = Integer;
+    fn sub(self, rhs: Self) -> Self::Output {
+        return add_router(&self,&-rhs);
+    }
+}
+
+impl SubAssign for Integer {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = add_router(self,&-rhs);
+    }
+}
+
+impl Mul for Integer {
+    type Output = Integer;
+    fn mul(self, rhs: Self) -> Self::Output {
+        return Integer {
+            sign: self.sign ^ rhs.sign,
+            abs_number: arbitrary_precision_mul(&self.abs_number, &rhs.abs_number),
+        };
+    }
+}
+
+impl MulAssign for Integer {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.sign = self.sign ^ rhs.sign;
+        self.abs_number = arbitrary_precision_mul(&self.abs_number, &rhs.abs_number);
+    }
+}
+
+impl PartialOrd for Integer {
+    fn ge(&self, other: &Self) -> bool {
+        
+    }
+    fn gt(&self, other: &Self) -> bool {
+        
+    }
+    fn le(&self, other: &Self) -> bool {
+        
+    }
+    fn lt(&self, other: &Self) -> bool {
+        
+    }
+}
+
+impl PartialEq for Integer {
+    fn eq(&self, other: &Self) -> bool {
+        if self.abs_number == other.abs_number {
+            return !(self.sign ^ other.sign) && self.abs_number== vec![0u64] 
+        }
+        return false;
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        return !self.eq(&other);
+    }
+}
+
+
+impl Div for Integer {
+    type Output = Rational;
+    fn div(self, rhs: Self) -> Self::Output {
+        todo!("Not Impremented!!!");
     }
 }
 
